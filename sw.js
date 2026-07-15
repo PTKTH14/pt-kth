30f5a662e6fd21e1d4761f53fdde3930a3da71e4
PTKTH14 <ptkth11199@gmail.com>
2026-07-15 18:38:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pt-kth-v20260714-header-typography-v2';
+const CACHE_NAME = 'pt-kth-v20260715-review-photos-v3';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', event => {
