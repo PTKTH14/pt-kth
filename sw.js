04b4bd198ee8574aed0642e007ccb842196b953e
PTKTH14 <ptkth11199@gmail.com>
2026-07-16 08:46:52 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-cconst CACHE_NAME = 'pt-kth-v20260715-header-title-v6';
+const CACHE_NAME = 'pt-kth-v20260716-time-checklist-dashboard-v8';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', event => {
