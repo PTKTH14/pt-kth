44d9921751f9252452dd9b39ec182d2ef2d5ace3
PTKTH14 <ptkth11199@gmail.com>
2026-07-28 23:34:17 +0700

    Remove redundant LINE action from retrospective history

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pt-kth-v20260728-deduplicated-ui-v54';
+const CACHE_NAME = 'pt-kth-v20260728-clean-history-v55';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', event => {
