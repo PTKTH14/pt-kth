a8e66ac8eb98d3d1cf4eed5c7b3755f70df57bbd
PTKTH14 <ptkth11199@gmail.com>
2026-07-28 22:10:32 +0700

    Add retrospective checklist review system

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pt-kth-v20260728-performance-qc-v47';
+const CACHE_NAME = 'pt-kth-v20260728-retrospective-v49';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', event => {
