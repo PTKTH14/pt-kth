6d53c445a78e7a35dc1b268eb53e8586b8ccaede
PTKTH14 <ptkth11199@gmail.com>
2026-07-28 22:51:19 +0700

    Refresh cache for mobile v51

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pt-kth-v20260728-line-share-v50';
+const CACHE_NAME = 'pt-kth-v20260728-mobile-more-v51';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', event => {
