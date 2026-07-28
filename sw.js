e82495c01814dd18ca956d0cdb1f5292f38f7318
PTKTH14 <ptkth11199@gmail.com>
2026-07-28 19:57:25 +0700

    Refresh cache for mobile review fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pt-kth-v20260726-fair-stock-confirm-v42';
+const CACHE_NAME = 'pt-kth-v20260728-touch-review-fix-v44-1';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', event => {
