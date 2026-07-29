321dc910a426f24ce964ae96eb5c6657ebf535e6
PTKTH14 <ptkth11199@gmail.com>
2026-07-29 16:40:42 +0700

    Fix iPhone input zoom and LINE browser guidance

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pt-kth-v20260729-retrospective-delete-v58';
+const CACHE_NAME = 'pt-kth-v20260729-mobile-input-line-v59';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', event => {
