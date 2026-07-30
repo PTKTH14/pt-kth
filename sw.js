27b9bc69edca37812e005858e4a1e04b4194dbde
PTKTH14 <ptkth11199@gmail.com>
2026-07-30 11:31:11 +0700

    Deploy v67 state-transition update fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pt-kth-v20260730-fast-safe-update-v66';
+const CACHE_NAME = 'pt-kth-v20260730-update-toast-fix-v67';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', event => {
