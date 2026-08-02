27f002f5c59f6e688376d01b7fe9012bdca50850
PTKTH14 <ptkth11199@gmail.com>
2026-08-02 13:58:42 +0700

    Fix dismissible help tooltips

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pt-kth-v20260802-dashboard-period-v81';
+const CACHE_NAME = 'pt-kth-v20260802-dismissible-help-v82';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', event => {
