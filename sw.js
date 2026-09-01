7a68b351235e1457443afd5021cc10f3ed504ed7
PTKTH14 <ptkth11199@gmail.com>
2026-09-01 18:18:58 +0700

    Fix Checklist submit button
    Declare the submit lock state and show visible progress while saving. QC passed 240 checks.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pt-kth-v20260901-desktop-line-qr-v98';
+const CACHE_NAME = 'pt-kth-v20260901-submit-fix-v99';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg', './qrcode.js'];
 
 self.addEventListener('install', event => {
